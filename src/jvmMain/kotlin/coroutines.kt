8d0ebe4dfb7b07d04ce86990ff6e378ee6095006
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-08-21 21:47:21 +0200

    Wed Aug 21 21:47:21 CEST 2024

--- src/jvmMain/kotlin/coroutines.kt
+++ src/jvmMain/kotlin/coroutines.kt
@@ -1,10 +1,11 @@
+import java.util.concurrent.Executors
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.TimeUnit
 import kotlin.coroutines.*
 
 fun main() {
     val coroutine = ::async.createCoroutine(Continuation(MainDispatcher) { result -> result.getOrThrow() })
-    MainDispatcher.dispatch {  coroutine.resume(Unit) }
+    MainDispatcher.dispatch { coroutine.resume(Unit) }
     MainDispatcher.loop()
 }
 
@@ -28,8 +29,15 @@ object MainDispatcher : Dispatcher {
     }
 
     fun loop() {
-        while(true) {
+        while (true) {
             queue.poll(1, TimeUnit.SECONDS)?.invoke() ?: return
         }
+    }
+}
+
+object BackgroundDispatcher : Dispatcher {
+    private val executors = Executors.newFixedThreadPool(4)
+    override fun dispatch(block: () -> Unit) {
+        executors.execute { block() }
     }
 }
